c72410708349f8a3714f9cf3924c1b98ff3f2c00
Li-JunXian <>
2025-01-24 10:29:31 +0800

    Add Increment: Level-0.Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Lintz User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Lintz.java
@@ -0,0 +1,21 @@
+public class Lintz {
+    public static void main(String[] args) {
+        String logo = " _       _            _                \n"
+                    + "| |     |_|  _      _| |_   _ _        \n"
+                    + "| |     | | | |__  |_   _| |__  |      \n"
+                    + "| |___  | | |  _  |  | |__  /  /       \n"
+                    + "|_____| |_| |_| |_|  |___/ /____|      \n";
+        String horiLine = "\u2500".repeat(100);
+
+        System.out.println(horiLine);
+        System.out.println(logo);
+        System.out.println(horiLine);
+
+        System.out.println("Hello! I'm Lintz");
+        System.out.println("What can I do for you?\n");
+        System.out.println(horiLine);
+
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(horiLine);
+    }
+}
