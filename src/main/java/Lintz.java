bf46ee8d3b4e1e539e379381fb0e06eaf335b352
LI JUNXIAN <141603348+Li-JunXian@users.noreply.github.com>
2025-01-31 12:52:30 +0800

    Add echo function to echo commands by user and exit when user input bye

--- src/main/java/Lintz.java
+++ src/main/java/Lintz.java
@@ -1,21 +1,45 @@
+import java.util.Scanner;
+
 public class Lintz {
+
+    private static String logo = " _       _            _                \n"
+            + "    | |     |_|  _      _| |_   _ _        \n"
+            + "    | |     | | | |__  |_   _| |__  |      \n"
+            + "    | |___  | | |  _  |  | |__  /  /       \n"
+            + "    |_____| |_| |_| |_|  |___/ /____|      \n";
+    private static String horiLine = "\u2500".repeat(100);
+
+    public static void greet(){
+        System.out.println("\t" + horiLine);
+        System.out.println("\t" + logo);
+        System.out.println("\t" + horiLine);
+
+        System.out.println("\t" + "Hello! I'm Lintz");
+        System.out.println("\t" + "What can I do for you?\n");
+        System.out.println("\t" + horiLine);
+    }
+
+    public static void echo(String input) {
+        System.out.println("\t" + horiLine);
+        System.out.println("\t" + input);
+        System.out.println("\t" + horiLine);
+    }
+
     public static void main(String[] args) {
-        String logo = " _       _            _                \n"
-                    + "| |     |_|  _      _| |_   _ _        \n"
-                    + "| |     | | | |__  |_   _| |__  |      \n"
-                    + "| |___  | | |  _  |  | |__  /  /       \n"
-                    + "|_____| |_| |_| |_|  |___/ /____|      \n";
-        String horiLine = "\u2500".repeat(100);
+        greet();
 
-        System.out.println(horiLine);
-        System.out.println(logo);
-        System.out.println(horiLine);
+        while (true){
+            String line;
+            Scanner input = new Scanner(System.in);
+            line = input.nextLine();
 
-        System.out.println("Hello! I'm Lintz");
-        System.out.println("What can I do for you?\n");
-        System.out.println(horiLine);
-
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println(horiLine);
+            if (line.toLowerCase().equals("bye")){
+                System.out.println("\t" + horiLine);
+                System.out.println("\t" + "Bye. Hope to see you again soon!");
+                System.out.println("\t" + horiLine);
+                break;
+            }
+            echo(line);
+        }
     }
 }
