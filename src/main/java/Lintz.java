b79fb9cd6208c7b20153933276aec752fbec2633
LI JUNXIAN <141603348+Li-JunXian@users.noreply.github.com>
2025-01-31 13:33:25 +0800

    Implement add and list functionalities to the chatbot for task management

--- src/main/java/Lintz.java
+++ src/main/java/Lintz.java
@@ -8,6 +8,8 @@ public class Lintz {
             + "    | |___  | | |  _  |  | |__  /  /       \n"
             + "    |_____| |_| |_| |_|  |___/ /____|      \n";
     private static String horiLine = "\u2500".repeat(100);
+    private static String[] taskList = new String[100];
+    private static int taskCount = 0;
 
     public static void greet(){
         System.out.println("\t" + horiLine);
@@ -20,9 +22,24 @@ public class Lintz {
     }
 
     public static void echo(String input) {
-        System.out.println("\t" + horiLine);
         System.out.println("\t" + input);
-        System.out.println("\t" + horiLine);
+    }
+
+    public static void taskListManager(String input) {
+        if (!input.equalsIgnoreCase("list")){
+            taskList[taskCount] = input;
+            System.out.println("\t" + horiLine);
+            System.out.print("\t" + "added: ");
+            echo(input);
+            System.out.println("\t" + horiLine);
+            taskCount += 1;
+        } else {
+            System.out.println("\t" + horiLine);
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println("\t" + (i + 1) + ". " + taskList[i]);
+            }
+            System.out.println("\t" + horiLine);
+        }
     }
 
     public static void main(String[] args) {
@@ -33,13 +50,13 @@ public class Lintz {
             Scanner input = new Scanner(System.in);
             line = input.nextLine();
 
-            if (line.toLowerCase().equals("bye")) {
+            if (line.equalsIgnoreCase("bye")) {
                 System.out.println("\t" + horiLine);
                 System.out.println("\t" + "Bye. Hope to see you again soon!");
                 System.out.println("\t" + horiLine);
                 break;
             }
-            echo(line);
+            taskListManager(line);
         }
     }
 }
